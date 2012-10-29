6d6cb94a56ed4e50d8d3f1df915b21795f74f277
Matt Kline <slavik262@gmail.com>
2012-10-29 08:51:51 -0500

    Added virtual destructor to Brush
    This removes a warning and is best practice,
    since it is an abstract class.

--- Brush.hpp
+++ Brush.hpp
@@ -10,6 +10,9 @@ public:
 	Brush(int radius)
 		: r(radius)
 	{ assert(r <= 15); }
+
+	virtual ~Brush() { }
+
 	int getRadius() const { return r; }
 
 	virtual unsigned char getValue(int x, int y) const = 0;
